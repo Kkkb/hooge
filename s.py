670cb6e3fd981934478fce3e3d63df0e8168e346
K_B <fengkunbin@126.com>
2018-04-16 15:27:27 +0800

    test route the relations of (url, endpoint, viewfunction)

--- s.py
+++ s.py
@@ -13,12 +13,17 @@ def index():
 #	return redirect(url_for('cookie'))
 #	abort(404)
 
-	if 'username' in session:
-		return 'Logged in as %s' % escape(session['username'])
-	return 'You are not logged in'
-
+#	if 'username' in session:
+#		return 'Logged in as %s' % escape(session['username'])
+#	return 'You are not logged in'
+#	return request.headers.get('Connection')
+	return render_template('index.html')
 app.secret_key ='\xa5/\x9dC\xf2\xd0,\xae\xb4\xb15\x85\x02a\xa7\x8c\x0fx\xd1)\x86\x8f&\x92'
 
+@app.route('/404')
+def page_not_found():
+	abort(404)
+	
 @app.route('/<username>')
 def show_user_profile(username=None):
 	return render_template('user.html', username=username)
@@ -54,7 +59,7 @@ def upload_file():
 		return render_template('upload.html')
 
 @app.route('/cookies')
-def cookie():
+def kcookie():
 	return request.cookies.get('username')
 
 @app.errorhandler(404)
@@ -62,3 +67,10 @@ def page_not_found(error):
 	resp = make_response(render_template('error.html'), 404)
 	resp.headers['X-Something'] = 'A value'
 	return resp
+
+if __name__ == '__main__':
+#	app.run(debug=True, host='0.0.0.0')
+#	print(app.view_functions)
+#	print(app.url_map)
+#	print(app.url_map._rules)
+	print(app.url_map._rules_by_endpoint)
